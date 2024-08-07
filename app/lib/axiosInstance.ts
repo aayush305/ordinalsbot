ecf30c5bbca7a86dd97bcf7469019058e45d8e7e
Aayush Dakwala <aayushdakwala@gmail.com>
2024-08-07 00:19:39 -0300

    Axios setup

---
+++ app/lib/axiosInstance.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.ORDINALSBOT_API_URL,
+  timeout: 10000,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => config,
+  (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.error("Axios Error:", error.response || error.message);
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
